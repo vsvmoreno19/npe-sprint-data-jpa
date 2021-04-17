c283bf5e0a491be3a8092e37ad0e4ef6adf32ca4
Valeria Scarlett Villarreal Moreno <valeria.villarreal@banregio.com>
2021-04-17 13:41:44 -0500

    Mi primer commit

---
+++ src/main/java/com/scar/npesprintdatajpa/NpeSprintDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.scar.npesprintdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpeSprintDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpeSprintDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scar/npesprintdatajpa/NpeSprintDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scar.npesprintdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpeSprintDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
